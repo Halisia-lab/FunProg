493bacaee52bdd3c563cf54e951fe736999607a9
Wissam BENADJAOUD <73896903+Wissam-Ben@users.noreply.github.com>
2023-02-03 12:24:52 +0100

    classes

---
+++ src/main/scala/progfun/models/Instruction.scala
@@ -0,0 +1,10 @@
+package progfun.models
+
+object Instruction {
+  val AVANCE = "A"
+  val GAUCHE = "G"
+  val DROITE = "D"
+
+  val instructions = List(AVANCE, GAUCHE, DROITE)
+
+}
---
+++ src/main/scala/progfun/models/Position.scala
@@ -0,0 +1,3 @@
+package progfun.models
+
+case class Position(coordinate: Coordinate, orientation: Orientation) {}
